1e685e03d8c75deaa2256e1721951ae3e5066384
salithab <salithabey@gmail.com>
2023-04-10 20:23:21 +0530

    Create photographer's profile

--- my-first-app/src/Pages/Photographer/photographer.js
+++ my-first-app/src/Pages/Photographer/photographer.js
@@ -1,37 +1,50 @@
-import React from "react"
-import "./photographer.css"
+import React from "react";
+import "./photographer.css";
 
 const Photographer = () => {
+  return (
+    <div className="profile">
+      <div className="upper">
+        <h3>Overview</h3>
+        <h3>About photographer</h3>
+        <h3>Reviews</h3>
+      </div>
 
-    return (
+      <div className="pName">
+        <h3>ROBERT FRANK</h3>
 
-        <div className="profile">
-            
-            <div className="upper">
-                <h3>Overview</h3>
-                <h3>About photographer</h3>
-                <h3>Reviews</h3>
-            </div>
+        <h6>Level 1 photographer</h6>
+      </div>
 
-            <div className="pName">
-                <h3>ROBERT FRANK</h3>
-
-                <h6>Level 1 photographer</h6>
-            </div>
-
-
-            <div className="about">
-
-                {/* <img className="pimg" src="./robert"/> */}
-
-                <p>I am a professional Wedding and Family Photographer 
-                    with over 8 years of experience retouching & postprocessing images from many different Genres like 
-                    Weddings, Baptism and others</p>
-            </div>
-
-
+      <div className="ptofileHead">
+        <div className="about">
+          <div className="profilePhoto">
+            <img className="pimg" src="./images/robert.jpg" />
+          </div>
+          <div className="description">
+            I am a professional Wedding and Family Photographer with over 8years
+            of experience retouching & postprocessing images from manydifferent
+            Genres like Weddings, Baptism and others
+          </div>
         </div>
-    )
-}
+        <div className="photoCollection">
+          <div className="collectionRow">
+            <div className="collectionCol">
+              <img className="mainImg" src="./images/robert.jpg"></img>
+            </div>
+          </div>
+          <div className="collectionRow">
+            <div className="collectionCol">
+              <img className="secImg" src="./images/robert.jpg" ></img>
+            </div>
+            <div className="collectionCol">
+              <img  className="secImg" src="./images/robert.jpg"></img>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Photographer
+export default Photographer;
